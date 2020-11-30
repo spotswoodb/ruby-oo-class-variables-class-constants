3d285e6813ddcf1505b69f35f8cbcbf34d65d5bd
Ben Spotswood <spotswoodb@gmail.com>
2020-11-30 16:56:06 -0600

    commit tree 9ba4151d20e7b73f08d9f88344911d3929e32138

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,24 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS.each do |brand|
+  #   BRANDS << brand
+  #   end
+  # end
+  # BRANDS.uniq!
 end
